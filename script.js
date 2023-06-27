4ce2371943ca8d04735346d63f1eb8264e28bc55
agomez99 <austine.gomez@yahoo.com>
2023-06-27 11:40:17 -0500

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello!');
+function subscribe() {
+  var email = document.getElementById('email').value;
+  
+  // Perform validation on the email address
+  if (!validateEmail(email)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+  
+  // Perform further processing (e.g., sending data to the server)
+  // ...
+  
+  // Display a success message
+  alert('Thank you for subscribing to our newsletter!');
+  
+  // Clear the input field
+  document.getElementById('email').value = '';
+}
+
+function validateEmail(email) {
+  // Use a regular expression pattern to validate the email address
+  var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return pattern.test(email);
+}
