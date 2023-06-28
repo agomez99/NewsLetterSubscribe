b3781f9f74516fe6d9fd56e2f76842e50d7f18bc
Austine Gomez <austine.gomez@yahoo.com>
2023-06-27 23:37:52 -0500

    CSS

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 function subscribe() {
-  var email = document.getElementById('email').value;
-  
-  // Perform validation on the email address
-  if (!validateEmail(email)) {
+  const emailInput = document.getElementById('email');
+  const successMessage = document.getElementById('success-message');
+  const content = document.getElementById('content');
+  const email = emailInput.value.trim();
+  const isValidEmail = validateEmail(email);
+
+  if (!isValidEmail) {
     alert('Please enter a valid email address.');
     return;
   }
 
-  document.getElementById('success-message').style.display = 'block';
-  document.getElementById('content').style.display = 'none';
-  document.getElementById('email').value = '';
-}
-function clearMessage(){
-  document.getElementById('email').value = '';
-  document.getElementById('success-message').style.display = 'none';
-  document.getElementById('content').style.display = 'flex';
-
+  successMessage.style.display = 'block';
+  content.style.display = 'none';
+  emailInput.value = '';
 }
 
 function validateEmail(email) {
-  // Use a regular expression pattern to validate the email address
-  var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return pattern.test(email);
 }
-     
+
+function clearMessage() {
+  const emailInput = document.getElementById('email');
+  const successMessage = document.getElementById('success-message');
+  const content = document.getElementById('content');
+  
+  emailInput.value = '';
+  successMessage.style.display = 'none';
+  content.style.display = 'flex';
+}
